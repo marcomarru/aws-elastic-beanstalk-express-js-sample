89f5edfbd2afa4c8e144a64bd8e992db14ff1623
marru <marcomarru@marrucci1983@gmail.com>
2023-11-17 14:22:19 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Puglia!'));
+app.get('/', (req, res) => res.send('Hello Taranto and Grottaglie!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
